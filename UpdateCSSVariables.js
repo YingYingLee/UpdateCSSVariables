1ea1c6a28e56706c47a773269256ec2a5cf8f80e
YingYingLee <yylee1123@gmail.com>
2017-01-11 10:16:59 +0800

    Update CSS Variables with JS

---
+++ UpdateCSSVariables.js
@@ -0,0 +1,10 @@
+
+function handleUpdate() {
+	const suffix = this.dataset.sizing || "";
+	document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+const inputs = document.querySelectorAll(".controls input");	//not array
+
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
